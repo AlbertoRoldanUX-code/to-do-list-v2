daaa65092eef70844e43c69aa71874c1c78b07a7
Alberto Roldán <a-f-r@hotmail.es>
2021-06-05 21:49:50 +0200

    Create NewTask component and add it in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Form from "./Form.js";
+import NewTask from "./NewTask.js";
 import Tasks from "./Tasks.js";
 
 function App() {
   return (
     <div>
-      <Form />
+      <NewTask />
       <Tasks />
     </div>
   );
